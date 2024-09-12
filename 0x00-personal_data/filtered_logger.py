7dd9a48f8f056669cf079c41a327fbdcbf36b82a
ZainAalabidin <zynly9303@gmail.com>
2024-09-12 11:33:00 +0000

    ok

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,12 +5,8 @@ from typing import List
 
 
 def filter_datum(
-        fields: List[str], redaction: str,
-        message: str, separator: str
+        fields: List[str], redaction: str, message: str,
+        separator: str
         ) -> str:
-    '''eturns the log message obfuscated'''
-    for field in fields:
-        message = re.sub(
-                f'{field}=(.*?){separator}',
-                f'{field}={redaction}{separator}', message)
-        return message
+    return re.sub(f"({'|'.join(fields)})=.*?{separator}",
+                  lambda m: f"{m.group(1)}={redaction}{separator}", message)
