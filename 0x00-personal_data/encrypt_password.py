0e15bd386adc142d2f9bcee612bc2878645d2bc3
ZainAalabidin <zynly9303@gmail.com>
2024-09-14 12:34:43 +0000

    ok

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''User passwords should NEVER be stored in plain text in a database.'''
+import bcrypt
+
+
+def hash_password(password) -> bytes:
+    '''function that expects one string argument name password
+    and returns a salted, hashed password'''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' validate that the provided password matches the hashed password.'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
